8b0814c8f0e4999c0d449e114b2000bcea2900b6
emmalanier <emma.lanier@outlook.fr>
2025-12-08 14:19:15 +0100

    Creation of class

--- includes/header.h
+++ includes/header.h
@@ -1,3 +1,33 @@
 /////////////////////////
 //MAGETISATION - HEADER//
 /////////////////////////
+
+#ifndef HEADER_H
+#define HEADER_H
+
+//INCLUDES//
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+//CLASSES//
+
+class magnetisation : public//classe d'integ
+{
+  private :
+    double h;
+    double g;
+    double taille;
+    double magnetisation;
+    double magnetisationInter;
+
+  public :
+    magnetisation();
+    magnetisation(double, double, double);
+    magnetisation(double, double, double, double, double);
+
+};
+
+
+#endif//HEADER_H
